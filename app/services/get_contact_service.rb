b77f370f1e3c6cfb10d1cdc2d96004de21ba91c3
fahad ishfaq <fahadishfaq18@gmail.com>
2020-07-21 16:59:03 +0500

    minor fix

--- app/services/get_contact_service.rb
+++ app/services/get_contact_service.rb
@@ -1,7 +1,7 @@
 class GetContactService
   attr_reader :contact_id, :base_url
 
-  ACCESS_TOKEN = "OMGHlFS1aKXXfRGFSwucnhLrAvJU"
+  ACCESS_TOKEN = "bV7CBT3uGnFsD600bgbrbWc3AlsN"
 
   def initialize(contact_id)
     @base_url = "https://api.infusionsoft.com/crm/rest/v1/contacts"
